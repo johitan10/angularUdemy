563bcbf34a9979aebba8d2cdde88c95b3aa5ad5d
johan <johan930131@gmail.com>
2018-09-18 23:30:23 -0500

    pipes

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+
+  nombre:string = "johan lopez";
+
+  arreglo = [1,2,3,4,5,6,7,8,9];
+
+  PI = Math.PI;
+
 }
